559dd994f51386f188daf76b5bec91d6b73135f3
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-09-29 16:49:31 -0600

    4.4 inicio

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import { TiposBasicos } from './typescript/TiposBasicos';
+//comentamos el import tipos basicos para la 4.4
+//import { TiposBasicos } from './typescript/TiposBasicos';
+
+//LLamamos al componente Obtejetos Literales
+import { ObjetosLiterales } from './typescript/ObjetosLiterales';
+
 const App = () => {
   return (
     <div>
      
       <h1>Introduccion a Type Script con React</h1>
       <hr />
-      <TiposBasicos/>
-     
+      {/*<TiposBasicos/>*/}
+      <ObjetosLiterales/>
     </div>
  
   )
