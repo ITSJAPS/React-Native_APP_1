adb84312dc8b441372cf79002be0435235e88a60
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-09-30 18:53:54 -0600

    4.10 ala mitad

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -6,51 +6,77 @@ export const Usuarios = () => {
 
     const [usuarios, setUsuarios] = useState<Usuario[]>([]);
 
+    const japsFNCargaUsuarios = ()=>{
+         //llamado de la API
+         reqRespApi.get<ReqRespUsuarioListado>('/users')
+         .then(resp=> {
+               //  console.log(resp.data.data[0].first_name);
+               //  console.log(resp.data.data)
+               //  console.log(resp.data.data[0].email)
+               //  console.log(resp.data.data)
+               setUsuarios(resp.data.data);
+             })
+         .catch(err => console.log(err))
+    }
+
+
     useEffect(() => {
-        //FIC: llamado de la API
-        reqRespApi.get<ReqRespUsuarioListado>('/users')
-        .then(resp=> {
-              //  console.log(resp.data.data[0].first_name);
-              //  console.log(resp.data.data)
-              //  console.log(resp.data.data[0].email)
-              //  console.log(resp.data.data)
-              //setUsuarios(resp.data.data);
-            })
-        .catch(err => console.log(err))
+       japsFNCargaUsuarios();
         }, [])
     const renderItem = (usuario: Usuario) => {
+        return (
+            <tr key = {usuario.id.toString()}>
+                <td>
+                    <img src={usuario.avatar} alt = {usuario.first_name}
+                    style = {{
+                        width: 50,
+                        borderRadius: 100
+                    }}
+                    ></img>
+                </td>
+                <td>
+                    {usuario.first_name} {usuario.last_name}
+                </td>
+                <td>
+                    {usuario.email}
+                </td>
+            </tr>
+            
+        )
+    }
+
+
     return (
-        <tr key={usuario.id.toString()}>
+        <>
             <h3>Usuarios</h3>
             <table className = "table">
-                <thead>
-                    <tr>
-                        <td>
-                            <img
-                                src={ usuario.avatar }
-                                alt={ usuario.first_name }
-                                style={{
-                                    width: 50,
-                                    borderRadius: 100
-                                }}
-                            >
-                            </img>
-                        </td>
-                        <th>
-                        { usuario.first_name } { usuario.last_name }
-                        </th>
-                        <th>
-                        { usuario.email }
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
+                    <thead>
+                          <tr>
+                                <th>
+                                    Avatar
+                                </th>
+                                <th>
+                                    Nombre
+                                </th>
+                                 <th>
+                                    email
+                                </th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                        {
                         usuarios.map(FicArgUsuario => renderItem(FicArgUsuario))
                     }
                 </tbody>
             </table>
-        </tr>
+            <button onClick={japsFNCargaUsuarios}>
+                Anterior
+            </button>
+
+            <button onClick = {japsFNCargaUsuarios}>
+                Siguiente
+            </button>
+        </>
     )
-    }
+    
 }
