bfaee59a92359f699b55bc114727c7d732d32f6a
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-09-30 18:30:44 -0600

    4.9 completo

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ const App = () => {
       {/*<ContadorHook/>*/}
       {/*<Login/>*/}
       {<Usuarios/>}
+      
     </div>
  
   )
--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,32 +1,56 @@
+import { useEffect, useState } from "react";
+import { reqRespApi } from '../api/reqRes';
+import { ReqRespUsuarioListado, Usuario } from '../interfaces/resResp';
+
 export const Usuarios = () => {
+
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
 
     useEffect(() => {
         //FIC: llamado de la API
-        reqRespApi.get('/users')
+        reqRespApi.get<ReqRespUsuarioListado>('/users')
         .then(resp=> {
-                console.log(resp);
-        })
+              //  console.log(resp.data.data[0].first_name);
+              //  console.log(resp.data.data)
+              //  console.log(resp.data.data[0].email)
+              //  console.log(resp.data.data)
+              //setUsuarios(resp.data.data);
+            })
         .catch(err => console.log(err))
         }, [])
-  
+    const renderItem = (usuario: Usuario) => {
     return (
-        <>
+        <tr key={usuario.id.toString()}>
             <h3>Usuarios</h3>
             <table className = "table">
                 <thead>
                     <tr>
+                        <td>
+                            <img
+                                src={ usuario.avatar }
+                                alt={ usuario.first_name }
+                                style={{
+                                    width: 50,
+                                    borderRadius: 100
+                                }}
+                            >
+                            </img>
+                        </td>
                         <th>
-                            Avatar
+                        { usuario.first_name } { usuario.last_name }
                         </th>
                         <th>
-                            Nombre
-                        </th>
-                        <th>
-                            email
+                        { usuario.email }
                         </th>
                     </tr>
                 </thead>
+                <tbody>
+                    {
+                        usuarios.map(FicArgUsuario => renderItem(FicArgUsuario))
+                    }
+                </tbody>
             </table>
-        </>
+        </tr>
     )
+    }
 }
