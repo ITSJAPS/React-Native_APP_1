2e4c3f764ba9b812ec3e9dff1a15a4ea543cc663
ITSJAPS <joalperezso@ittepic.edu.mx>
2022-09-30 20:41:18 -0600

    4.13 ya :(

--- src/components/Formularios.tsx
+++ src/components/Formularios.tsx
@@ -1,26 +1,9 @@
 import { useState } from "react"
+import { useForm } from './hooks/useForm';
 
 export const Formularios = () => {
-
-    const [formulario, setformulario] = useState({
-        email: 'test@test.com',
-        password: '123456'
-    })
-    const onChange = (value:string, campo:string) => {
-        // llamado a la función que cambia el useState
-        setformulario({
-            // desestructuramos los valores del useState
-            ...formulario,
-            // aqui es como si estuvieramos mandando
-            //el atributo email o password. Dicho de otro modo
-            //los [] computan es decir compara el valor que trae
-            //el parametro "campo" y lo relaciona con el valor del
-            //useState. Por ultimo si encontro un atributo que se llame
-            //igual le asigna el valor que trae el parametro "value"
-            //de la funcion onChange.
-            [campo]: value
-        });
-    }
+    const {formulario, onChange} = useForm()
+    
 
     return (
         <>
